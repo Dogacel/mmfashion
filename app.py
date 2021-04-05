ff281401a04140bb65980101ad88225c3b713f07
Doğaç Eldenk <dogacel@gmail.com>
2021-04-05 17:21:43 +0300

    Add color support

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ from mmfashion.core import AttrPredictor, CatePredictor
 from mmfashion.models import build_predictor
 from mmfashion.utils import get_img_tensor
 
+import colorgram, webcolors
+
 app = Flask(__name__)
 CORS(app)
 
@@ -32,6 +34,23 @@ def jsonResult(pred, idx2name):
 
     return myDict
 
+def closest_color(requested_color):
+    min_colors = {}
+    for key, name in webcolors.css3_hex_to_names.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_color[0]) ** 2
+        gd = (g_c - requested_color[1]) ** 2
+        bd = (b_c - requested_color[2]) ** 2
+        min_colors[(rd + gd + bd)] = name
+    return min_colors[min(min_colors.keys())]
+
+def get_color_name(requested_color):
+    try:
+        closest_name = actual_name = webcolors.rgb_to_name(requested_color)
+    except ValueError:
+        closest_name = closest_color(requested_color)
+        actual_name = None
+    return actual_name, closest_name
 
 @app.route('/annotate', methods=(['POST']))
 def annotate():
@@ -49,9 +68,13 @@ def annotate():
         coarse_attr_prob, coarse_attr_predictor.attr_idx2name)
     cats = jsonResult(cate_prob, cate_predictor.cate_idx2name)
 
+    colors = colorgram.extract(file, 5)
+    color_names = [get_color_name(x)[1] for x in colors]
+
     resultDict = {}
     resultDict['attributes'] = coarse_attrs
     resultDict['categories'] = cats
+    resultDict['colors'] = color_names
 
     return jsonify(resultDict)
 
